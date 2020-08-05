e8ba66f9ccbd5bbb05b4c59c963db0f52f5dd156
Logicjoker{} <59412154+logicjoker@users.noreply.github.com>
2020-08-05 20:41:07 +0700

    upload file

---
+++ cfg.php
@@ -0,0 +1,9 @@
+<?php
+
+// https://coindailytimes.com
+// Anjing kalian semua cokkk
+
+$ua = "xxxxxx";
+$session = "xxxxx";
+$__cfduid = "xxxxxx";
+$data = "&coins=doge&coins=trx&coins=ltc&coins=btc";
